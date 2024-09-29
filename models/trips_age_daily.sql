5782b00a2a180488e4edaa7320880a01f067cf52
tamara_s <tss.forstudy@gmail.com>
2024-09-29 12:21:31 +0000

    feat: incremental table trips_users

--- models/trips_age_daily.sql
+++ models/trips_age_daily.sql
@@ -1,16 +1,9 @@
-with trips_users as (
-	select 
-		*
-	from {{ref("trips_prep")}} t
-	join {{source("scooters_raw", "users")}} u
-	on t.user_id = u.id 
-)
-	select 
-		started_at::date as date, 
-		extract(year from age(started_at::date, birth_date)) user_age,
-		count(*) as trips,
-        sum(price_rub) as revenue_rub
-	from trips_users 
-	group by 
-		started_at::date, 
-		extract(year from age(started_at::date, birth_date)) 
+select 
+   date, 
+    age,
+    count(*) as trips,
+    sum(price_rub) as revenue_rub
+from {{ ref("trips_users") }} 
+group by 
+    started_at::date, 
+    extract(year from age(started_at::date, birth_date)) 
