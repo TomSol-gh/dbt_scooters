7c98faf47772de990a2e72704dda43ab2d4ca457
tamara_s <tss.forstudy@gmail.com>
2024-09-29 12:53:27 +0000

    feat: changed column names

--- models/trips_age_daily.sql
+++ models/trips_age_daily.sql
@@ -5,5 +5,5 @@ select
     sum(price_rub) as revenue_rub
 from {{ ref("trips_users") }} 
 group by 
-    started_at::date, 
-    extract(year from age(started_at::date, birth_date)) 
+    date,
+    age
--- models/trips_age_daily_stat.sql
+++ models/trips_age_daily_stat.sql
@@ -1,6 +1,6 @@
 select 
-	user_age,
+	age,
 	avg(trips) as avg_trips,
     avg(revenue_rub) as avg_revenue_rub
 from {{ref("trips_age_daily")}}
-group by user_age
+group by age
